17537d4fd411a1c7ec45c2ecbff1c6eeb582dbe2
Luke Arndt <luke.j.arndt@gmail.com>
2012-09-02 23:18:37 +1000

    Played around with Spike 3 stuff. Wrote some code to read from a CachedRowSet. Couldn't test; mySQL playing up.

--- src/dataAccessLayer/Spike3Stuff.java
+++ src/dataAccessLayer/Spike3Stuff.java
@@ -1,5 +1,45 @@
 package dataAccessLayer;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.sql.rowset.CachedRowSet;
+
+import com.sun.rowset.CachedRowSetImpl;
+
 public class Spike3Stuff {
 
+	CachedRowSet myCachedRowSet;
+	Statement myStatement;
+	ResultSet myResultSet;
+
+	public Spike3Stuff() throws SQLException {
+		try {
+			myCachedRowSet = new CachedRowSetImpl();
+			myStatement = Database.getDatabaseConnection().createStatement();
+			myResultSet = myStatement.executeQuery("SELECT * from member");
+			myCachedRowSet.populate(myResultSet);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (myCachedRowSet != null) {
+				myCachedRowSet.close();
+			}
+			if (myStatement != null) {
+				myStatement.close();
+			}
+			if (myResultSet != null) {
+				myResultSet.close();
+			}
+		}
+
+		System.out.println("Selecting from table ["
+				+ myCachedRowSet.getTableName() + "]");
+
+		while (myCachedRowSet.next()) {
+			System.out.println("Row number [" + myCachedRowSet.getRow() + "]");
+		}
+	}
 }
--- src/presentationLayer/RosterSystem.java
+++ src/presentationLayer/RosterSystem.java
@@ -1,8 +1,9 @@
 package presentationLayer;
+
 import java.sql.SQLException;
 
 import dataAccessLayer.Database;
-
+import dataAccessLayer.Spike3Stuff;
 
 /**
  * @author Luke Arndt & Deborah Harrington
@@ -24,31 +25,38 @@ public class RosterSystem {
 							+ e.getMessage());
 			System.err.println("Error code: " + e.getErrorCode());
 		}
-		
-		try {
-			String command = args[0];
-		
-			ConsoleUserInteraction consoleUserInteraction = new ConsoleUserInteraction();
-		
-			if (command.equals("insert")) {
-				consoleUserInteraction.performInsert();
-			} else if (command.equals("update")) {
-				consoleUserInteraction.performUpdate();
-			} else if (command.equals("delete")) {
-				consoleUserInteraction.performDelete();
-			} else if (command.equals("list")) {
-				consoleUserInteraction.getList();
-			} else if (command.equals("find")) {
-				consoleUserInteraction.performSelect();
-			} else {
-				System.out.println("You didn't enter a recognised command. The following are valid commands: " +
-						"insert, update, delete, list, find. Run the program again with a valid command");
-			}
-		
 
-		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("You did not specifiy a command for the program to carry out");
-			System.out.println(e.getMessage());
+		/*
+		 * try { String command = args[0];
+		 * 
+		 * ConsoleUserInteraction consoleUserInteraction = new
+		 * ConsoleUserInteraction();
+		 * 
+		 * if (command.equals("insert")) {
+		 * consoleUserInteraction.performInsert(); } else if
+		 * (command.equals("update")) { consoleUserInteraction.performUpdate();
+		 * } else if (command.equals("delete")) {
+		 * consoleUserInteraction.performDelete(); } else if
+		 * (command.equals("list")) { consoleUserInteraction.getList(); } else
+		 * if (command.equals("find")) { consoleUserInteraction.performSelect();
+		 * } else { System.out.println(
+		 * "You didn't enter a recognised command. The following are valid commands: "
+		 * +
+		 * "insert, update, delete, list, find. Run the program again with a valid command"
+		 * ); }
+		 * 
+		 * 
+		 * } catch (ArrayIndexOutOfBoundsException e) { System.out.println(
+		 * "You did not specifiy a command for the program to carry out");
+		 * System.out.println(e.getMessage()); }
+		 */
+
+		try {
+			@SuppressWarnings("unused")
+			Spike3Stuff mySpike3 = new Spike3Stuff();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
