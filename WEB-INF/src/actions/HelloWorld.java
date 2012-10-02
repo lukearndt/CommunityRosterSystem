252c2ff502a190a3e807eb93439e934e0b08f9d7
Luke Arndt <luke.j.arndt@gmail.com>
2012-10-02 23:33:04 +1000

    Added a CSS template, not that it's working...

--- WEB-INF/src/actions/HelloWorld.java
+++ WEB-INF/src/actions/HelloWorld.java
@@ -1,20 +1,21 @@
 package actions;
 
-import com.opensymphony.xwork2.ActionSupport; 
+import com.opensymphony.xwork2.ActionSupport;
 
 public class HelloWorld extends ActionSupport {
-  private String message;
+	private static final long serialVersionUID = 4466576904807045268L;
+	private String message;
 
-  public String getCool() {
-	  return "AWESOME";
-  }
-  
-  public String getMessage() {
-    return message;
-  }
+	public String getCool() {
+		return "AWESOME";
+	}
 
-  public String execute() {
-    message = "Hello World!";
-    return SUCCESS;
-  }
+	public String getMessage() {
+		return message;
+	}
+
+	public String execute() {
+		message = "Hello World!";
+		return SUCCESS;
+	}
 }
