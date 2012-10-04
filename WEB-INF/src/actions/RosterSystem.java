bfc2a108eac4eba393590097308bdb621b9d406b
Deborah <Deborah@136.186.192.196>
2012-10-04 11:45:04 +1000

    Work in progress

--- WEB-INF/src/actions/RosterSystem.java
+++ WEB-INF/src/actions/RosterSystem.java
@@ -2,18 +2,67 @@ package actions;
 
 import com.opensymphony.xwork2.ActionSupport;
 
+import java.sql.SQLException;
+import java.util.HashMap;
+import models.Capability;
+import models.DbDetails;
+import models.Duty;
+import models.Member;
+import net.java.ao.EntityManager;
+
+
+/**
+ * @author Luke Arndt & Deborah Harrington
+ * @date 20 September 2012
+ * 
+ */
 public class RosterSystem extends ActionSupport {
 
-	public RosterSystem() {
-		// TODO Auto-generated constructor stub
-	}
+	private static final long serialVersionUID = 4944803812385887371L;
+	
+	protected static EntityManager manager = new EntityManager(DbDetails.getDatabaseUrl(),
+			DbDetails.getUserName(), DbDetails.getPassword());;
 
 	/**
-	 * @param args
+	 * 
+	 * @param memberInformation
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void addMember(HashMap<String, Object> memberInformation) {
+		Member newMember;
+		try {
+			newMember = manager.create(Member.class, memberInformation);
+			newMember.save();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
-}
+	public static void updateMember(String name,
+			HashMap<String, Object> memberInformation) {
+		Member[] member;
+		try {
+			member = manager.find(Member.class, "name = ?", name);
+			for (int i = 0; i < member.length; i++) {
+				member[i].setName(memberInformation.get("Name").toString());
+				member[i].setAddress(memberInformation.get("address")
+						.toString());
+				member[i].setSuburb(memberInformation.get("suburb").toString());
+				member[i].setState(memberInformation.get("state").toString());
+				member[i].setPostCode(memberInformation.get("postcode")
+						.toString());
+				member[i].setHome_Phone(memberInformation.get("home_Phone")
+						.toString());
+				member[i].setMobile_Phone(memberInformation.get("mobile_Phone")
+						.toString());
+				member[i].save();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void deleteMember(String memberToDelete) {
+		Member[] member;
+		try {
--- WEB-INF/src/models/DbDetails.java
+++ WEB-INF/src/models/DbDetails.java
@@ -7,10 +7,28 @@ package models;
  */
 public abstract class DbDetails  {
 
-	protected static final String PASSWORD = "lk7^t5$FbN3$";
-	protected static final String USER_NAME = "SystemUser";
+	private static final String PASSWORD = "lk7^t5$FbN3$";
+	private static final String USER_NAME = "SystemUser";
 	protected static final String DATABASE_DRIVER = "com.mysql.jdbc.Driver";
-	protected static final String DATABASE_URL = "jdbc:mysql://localhost:3307/duty_roster";
+	private static final String DATABASE_URL = "jdbc:mysql://localhost:3307/duty_roster";
+	/**
+	 * @return the databaseUrl
+	 */
+	public static String getDatabaseUrl() {
+		return DATABASE_URL;
+	}
+	/**
+	 * @return the userName
+	 */
+	public static String getUserName() {
+		return USER_NAME;
+	}
+	/**
+	 * @return the password
+	 */
+	public static String getPassword() {
+		return PASSWORD;
+	}
 
 }
  
